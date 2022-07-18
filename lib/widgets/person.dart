f9823708a2fa45b3278002e182ccf5b4adc0cfb9
TruongDinhTho <43953312+TruongDinhTho@users.noreply.github.com>
2022-07-19 00:35:37 +0700

    Hoan thanh xong giao dien

--- lib/widgets/person.dart
+++ lib/widgets/person.dart
@@ -35,7 +35,7 @@ class _PersonListState extends State<PersonList> {
           stream: personBloc.subject.stream,
           builder: (context, AsyncSnapshot<PersonResponse> snapshot) {
             if (snapshot.hasData) {
-              if (snapshot.data!.error != null &&
+              if (snapshot.data!.error == null &&
                   snapshot.data!.error.length > 0) {
                 return _buildErrorWidget(snapshot.data!.error);
               }
